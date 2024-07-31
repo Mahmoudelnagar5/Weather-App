e743a30ad429a3ee073baccfb5e369c23fd0e98a
Mahmoud Elnagar <126767234+Mahmoudelnagar5@users.noreply.github.com>
2024-07-31 16:08:13 +0300

    Delete lib directory

--- lib/cubits/get_weather_cubit/weather_states.dart
+++
@@ -1,9 +0,0 @@
-class WeatherStates {}
-
-class WeatherInitialState extends WeatherStates {}
-
-class WeatherSuccessState extends WeatherStates {
-  WeatherSuccessState();
-}
-
-class WeatherFailureState extends WeatherStates {}
